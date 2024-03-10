0fcd6dc6119aaa097e8f4e0a0342be60016dabce
dydko <miroslaw.dyduch@gmail.com>
2024-03-10 09:02:29 +0100

    courses

--- src/test/java/streams/StreamComparator.java
+++ src/test/java/streams/StreamComparator.java
@@ -6,11 +6,15 @@ import org.testng.annotations.Test;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamComparator {
     @Test
     public void streamComparator() {
         List<Instructor> list = new Instructors().getALL().stream()
-                .sorted(Comparator.comparing());
+                .sorted(Comparator.comparing(Instructor::getName))
+                .collect(Collectors.toList());
+
+        list.forEach(System.out::println);
     }
 }
